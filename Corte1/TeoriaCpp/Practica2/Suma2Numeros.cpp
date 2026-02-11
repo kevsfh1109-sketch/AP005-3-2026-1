c1e69740e37997669e6003792082102faea6bb1c
kevsfh1109-sketch <kevsfh1109@gmail.com>
2026-02-11 15:31:08 -0500

    Add files via upload

---
+++ Corte1/TeoriaCpp/Practica2/Suma2Numeros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	double num1, num2, suma;
+	cout << "ingrese el primer numero: ";
+	cin >> num1;
+	cout << "ingrese el segundo numero: ";
+	cin >> num2;
+
+ 	suma=num1+num2;
+
+	cout << "la suma es: " <<suma <<endl;
+
+return 0;
+}
